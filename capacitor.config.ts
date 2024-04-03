f032baa2277c450c2f64fd153eead6967dc33a08
Perchant <gdavid@parrot-ctfs.com>
2024-04-03 13:22:56 +0200

    gymcar.pro + app works

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.gymcar.pro',
   appName: 'gymcar',
-  webDir: 'www',
+  webDir: 'build',
   server: {
     androidScheme: 'https'
   }
